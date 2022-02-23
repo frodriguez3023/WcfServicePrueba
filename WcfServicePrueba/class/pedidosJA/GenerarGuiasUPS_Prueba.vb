b861f36f0adc35e7b2092677179ad233039f7320
cronometraje <jasistemas@hotmail.com>
2022-02-22 22:42:46 -0600

    Fer, he agregado un webservice de prueba en mi rama, que debiera ser agregado al .SVC para su llamada, sin parametros de entrada y con response como string

---
+++ WcfServicePrueba/class/pedidosJA/GenerarGuiasUPS_Prueba.vb
@@ -0,0 +1,16 @@
+﻿Public Class GenerarGuiasUPS_Prueba
+    Public Function generandoGuias() As String
+        '-----------------------------
+        ' Este es un nuevo "controller" para un webservice SUGERIDO de generar guias XXX
+        ' Juan Arturo Gomez Perez Feb 2022
+        ' Tomado de https://www.youtube.com/watch?v=nEzeMdzA_Wk
+        '-----------------------------
+        Dim cadenaRespuesta As String = ""
+        cadenaRespuesta = "001"
+        cadenaRespuesta = cadenaRespuesta & " HOLA JUAN"
+        generandoGuias = cadenaRespuesta
+    End Function
+
+
+
+End Class
